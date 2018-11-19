49caa286eee4745106fcb26a86c2ba007d836826
1399852153 <139985215.@qq.com>
2018-11-19 22:01:31 +0800

    线性表内部实现

--- src/main/java/com/xiongyx/datastructures/list/ArrayList.java
+++ src/main/java/com/xiongyx/datastructures/list/ArrayList.java
@@ -17,7 +17,7 @@ public class ArrayList <E> implements List <E>{
     /**
      * 线性表默认的容量大小
      * */
-    protected static final int DEFAULT_CAPACITY = 16;
+    private static final int DEFAULT_CAPACITY = 16;
 
     /**
      * 内部数组的实际大小
@@ -52,21 +52,43 @@ public class ArrayList <E> implements List <E>{
     }
 
     //===============================================内部辅助方法===================================================
+    /**
+     * 下标越界检查
+     * @param index 下标值
+     */
+    private void rangeCheck(int index){
+        //:::如果下标小于0或者超过了最大值，抛出异常
+        if(index > this.size || index < 0){
+            throw new RuntimeException("index error  index=" + index + " size=" + this.size) ;
+        }
+    }
 
     //=================================================外部接口=======================================================
-
+    /**
+     * 返回线性表 内部元素的个数
+     * */
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
+    /**
+     * 线性表 是否为空
+     * @return true: 为空;
+     *          false: 不为空;
+     * */
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
+    /**
+     * 在线性表的末尾插入元素"e"
+     * @param e 需要插入的元素
+     * @return  插入成功，返回true；否则返回false
+     * */
     @Override
-    public boolean add(Object o) {
+    public boolean add(E e) {
         return false;
     }
 
