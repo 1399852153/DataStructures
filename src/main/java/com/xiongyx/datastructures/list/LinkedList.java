f7efcd9c5e90b6cc31e12367fc7e4f017666e46e
1399852153 <139985215.@qq.com>
2018-11-25 16:26:27 +0800

    链表内部实现--常用接口

--- src/main/java/com/xiongyx/datastructures/list/LinkedList.java
+++ src/main/java/com/xiongyx/datastructures/list/LinkedList.java
@@ -1,17 +1,130 @@
 package com.xiongyx.datastructures.list;
 
+import com.xiongyx.datastructures.iterator.Iterator;
+
 /**
  * @Author xiongyx
  * on 2018/11/25.
  *
  * 列表---链表实现
  */
-public class LinkedList <T> {
+public class LinkedList <E> implements List <E>{
+    /**
+     * 链表 头部哨兵节点
+     * */
+    private Node<E> first;
+    /**
+     * 链表 尾部哨兵节点
+     * */
+    private Node<E> last;
+    /**
+     * 链表 逻辑大小
+     * */
+    private int size;
+
+    public LinkedList() {
+        first = new Node<>();
+        last = new Node<>();
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (this.size == 0);
+    }
+
+    @Override
+    public int indexOf(E e) {
+        //:::当前节点 = 列表头部哨兵
+        Node currentNode = this.first;
+
+        if(e != null){
+            //:::如果不是查询空元素
+
+            //:::遍历列表
+            for(int i=0; i<this.size; i++){
+                //:::不断切换当前节点 ==> 当前节点 = 当前节点的右节点
+                currentNode = currentNode.right;
+                //:::如果满足要求（注意: equals顺序不能反，否则可能出现空指针异常）
+                if(e.equals(currentNode.data)){
+                    //:::返回下标
+                    return i;
+                }
+            }
+        }else{
+            //:::如果是查询null元素
+
+            //:::遍历列表
+            for(int i=0; i<this.size; i++){
+                //:::不断切换当前节点 ==> 当前节点 = 当前节点的右节点
+                currentNode = currentNode.right;
+                //:::如果满足要求
+                if(currentNode.data == null){
+                    //:::返回下标
+                    return i;
+                }
+            }
+        }
+
+        //:::遍历列表未找到相等的元素，返回特殊值"-1"代表未找到
+        return -1;
+    }
+
+    @Override
+    public boolean contains(E e) {
+        //:::复用indexOf方法,如果返回-1代表不存在;反之，则代表存在
+        return (indexOf(e) != -1);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, E e) {
+
+    }
+
+    @Override
+    public boolean remove(E e) {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E e) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 
     /**
      * 链表内部节点
      */
-    private static class Node <T>{
+    private static class Node <E>{
         /**
          * 左边关联的节点引用
          * */
@@ -25,13 +138,13 @@ public class LinkedList <T> {
         /**
          * 节点存储的数据
          * */
-        T data;
+        E data;
 
         //===================================内部节点 构造函数==================================
 
         public Node() {}
 
-        public Node(Node left, Node right, T data) {
+        public Node(Node left, Node right, E data) {
             this.left = left;
             this.right = right;
             this.data = data;
