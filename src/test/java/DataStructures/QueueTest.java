9eca59f95cfd22cfa5712f811f5ee6c2a7997392
xiongyx <1399852153@qq.com>
2018-12-08 13:56:30 +0800

    双端队列--基于数组的实现 bug修复

---
+++ src/test/java/DataStructures/QueueTest.java
@@ -0,0 +1,42 @@
+package DataStructures;
+
+import com.xiongyx.datastructures.queue.ArrayDeque;
+import com.xiongyx.datastructures.queue.Deque;
+
+public class QueueTest {
+
+    public static void main(String[] args){
+        testQueue();
+    }
+
+    private static void testQueue(){
+        Deque<Integer> deque = new ArrayDeque<>(3);
+        deque.addHead(1);
+        deque.addHead(2);
+        deque.addHead(3);
+        deque.addHead(4);
+        deque.addHead(5);
+        deque.addHead(6);
+        deque.addHead(7);
+        deque.addTail(101);
+        deque.addTail(102);
+        deque.addTail(103);
+        deque.addTail(104);
+        deque.addTail(105);
+        deque.addTail(106);
+
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.peekHead());
+        System.out.println(deque.peekTail());
+        System.out.println(deque.removeHead());
+        System.out.println(deque.removeHead());
+        System.out.println(deque.removeHead());
+        System.out.println(deque.removeTail());
+
+        System.out.println(deque.peekHead());
+        System.out.println(deque.peekTail());
+
+        deque.clear();
+        System.out.println(deque.isEmpty());
+    }
+}
--- src/test/java/DataStructures/StackTest.java
+++ src/test/java/DataStructures/StackTest.java
@@ -10,35 +10,6 @@ public class StackTest {
 
     public static void main(String[] args){
 //        testParenthesisMatching();
-
-        Deque<Integer> deque = new ArrayDeque<>(3);
-        deque.addHead(1);
-        deque.addHead(2);
-        deque.addHead(3);
-        deque.addHead(4);
-        deque.addHead(5);
-        deque.addHead(6);
-        deque.addHead(7);
-        deque.addTail(101);
-        deque.addTail(102);
-        deque.addTail(103);
-        deque.addTail(104);
-        deque.addTail(105);
-        deque.addTail(106);
-
-        System.out.println(deque.isEmpty());
-        System.out.println(deque.peekHead());
-        System.out.println(deque.peekTail());
-        System.out.println(deque.removeHead());
-        System.out.println(deque.removeHead());
-        System.out.println(deque.removeHead());
-        System.out.println(deque.removeTail());
-
-        System.out.println(deque.peekHead());
-        System.out.println(deque.peekTail());
-
-        deque.clear();
-        System.out.println(deque.isEmpty());
     }
 
     private static void testStack(){
