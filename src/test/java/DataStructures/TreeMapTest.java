aaa5e6e14b60b2224f3bda9d09bcc74658aefd69
1399852153 <139985215.@qq.com>
2019-01-15 21:17:50 +0800

    二叉搜索树 实现

--- src/test/java/DataStructures/TreeMapTest.java
+++ src/test/java/DataStructures/TreeMapTest.java
@@ -9,13 +9,15 @@ import com.xiongyx.datastructures.map.TreeMap;
 public class TreeMapTest {
     public static void main(String[] args){
         TreeMap<Integer,String> treeMap = new TreeMap<>();
+        treeMap.put(4,"4");
+        treeMap.put(3,"3");
+        treeMap.put(6,"6");
+        treeMap.put(2,"2");
         treeMap.put(1,"1");
-        treeMap.put(7,"1");
-        treeMap.put(5,"1");
-        treeMap.put(2,"1");
-        treeMap.put(4,"1");
-        treeMap.put(6,"1");
+        treeMap.put(5,"5");
+        CommonUtil.show(treeMap);
 
+        treeMap.remove(2);
         CommonUtil.show(treeMap);
     }
 }
